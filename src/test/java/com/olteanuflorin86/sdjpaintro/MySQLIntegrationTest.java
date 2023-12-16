ec234546e3bedd6447882d81f21df53c287b2d71
Florin Olteanu <olteanuflorin86@gmail.com>
2023-12-16 11:19:40 +0200

    Add aditional ITs for the UUIDs

--- src/test/java/com/olteanuflorin86/sdjpaintro/MySQLIntegrationTest.java
+++ src/test/java/com/olteanuflorin86/sdjpaintro/MySQLIntegrationTest.java
@@ -9,10 +9,13 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.test.context.ActiveProfiles;
 
+import com.olteanuflorin86.sdjpaintro.repositories.AuthorUuidRepository;
 import com.olteanuflorin86.sdjpaintro.repositories.BookNaturalRepository;
 import com.olteanuflorin86.sdjpaintro.repositories.BookRepository;
-
+import com.olteanuflorin86.sdjpaintro.repositories.BookUuidRepository;
+import com.olteanuflorin86.sdjpaintro.domain.AuthorUuid;
 import com.olteanuflorin86.sdjpaintro.domain.BookNatural;
+import com.olteanuflorin86.sdjpaintro.domain.BookUuid;
 
 @ActiveProfiles("local")
 @DataJpaTest
@@ -24,6 +27,12 @@ public class MySQLIntegrationTest {
 	BookRepository bookRepository;
 	
 	@Autowired
+    AuthorUuidRepository authorUuidRepository;
+
+    @Autowired
+    BookUuidRepository bookUuidRepository;
+	
+	@Autowired
 	BookNaturalRepository bookNaturalRepository;
 	
 	@Test
@@ -31,6 +40,27 @@ public class MySQLIntegrationTest {
 		long count = bookRepository.count();
 		assertThat(count).isEqualTo(2);
 	}
+	
+    @Test
+    void testBookUuid() {
+        BookUuid bookUuid = bookUuidRepository.save(new BookUuid());
+        assertThat(bookUuid).isNotNull();
+        assertThat(bookUuid.getId());
+
+        BookUuid fetched = bookUuidRepository.getById(bookUuid.getId());
+        assertThat(fetched).isNotNull();
+    }
+
+    @Test
+    void testAuthorUuid() {
+        AuthorUuid authorUuid = authorUuidRepository.save(new AuthorUuid());
+        assertThat(authorUuid).isNotNull();
+        assertThat(authorUuid.getId()).isNotNull();
+
+        AuthorUuid fetched = authorUuidRepository.getById(authorUuid.getId());
+        assertThat(fetched).isNotNull();
+    }
+
 	
 	@Test
 	void bookNaturalTest() {
