568fa733e72bfc8d320a4d785aedf9c9778c7d0c
Florin Olteanu <olteanuflorin86@gmail.com>
2023-12-18 17:19:00 +0200

    implement Get author by id

--- src/main/java/com/olteanuflorin86/sdjpaintro/dao/AuthorDaoImpl.java
+++ src/main/java/com/olteanuflorin86/sdjpaintro/dao/AuthorDaoImpl.java
@@ -1,15 +1,50 @@
 package com.olteanuflorin86.sdjpaintro.dao;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.sql.DataSource;
+
 import org.springframework.stereotype.Component;
 
 import com.olteanuflorin86.sdjpaintro.domain.Author;
 
 @Component
 public class AuthorDaoImpl implements AuthorDao {
+	
+	private final DataSource dataSource;
+	
+	public AuthorDaoImpl(DataSource dataSource) {
+		this.dataSource = dataSource;
+	}
 
 	@Override
 	public Author getById(Long id) {
-		// TODO Auto-generated method stub
+
+		Connection connection = null;
+		Statement statement = null;
+		ResultSet resultSet = null;
+		
+		try {
+			connection = dataSource.getConnection();
+			statement = connection.createStatement();
+			resultSet = statement.executeQuery("SELECT * FROM author WHERE id = " + id);
+			
+			if(resultSet.next()) {
+				Author author = new Author();
+				author.setId(id);
+				author.setFirstName(resultSet.getString("first_name"));
+				author.setLastName(resultSet.getString("last_name"));
+				
+				return author;
+			}
+			
+		} catch(SQLException e) {
+			e.printStackTrace();
+		}
+		
 		return null;
 	}
 
