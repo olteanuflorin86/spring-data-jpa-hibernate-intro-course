6c5ca346cd87d8756186d4310f77bbe01c68488e
Florin Olteanu <olteanuflorin86@gmail.com>
2023-12-20 19:40:26 +0200

    save new author

--- src/test/java/com/olteanuflorin86/sdjpaintro/DaoIntegrationTest.java
+++ src/test/java/com/olteanuflorin86/sdjpaintro/DaoIntegrationTest.java
@@ -121,6 +121,7 @@ public class DaoIntegrationTest {
         Author saved = authorDao.saveNewAuthor(author);
 
         assertThat(saved).isNotNull();
+        assertThat(saved.getId()).isNotNull();
     }
 
     @Test
