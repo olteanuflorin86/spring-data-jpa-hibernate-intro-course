39ec2a348c13304daf49e9d06a5fd49dd7e3954f
Florin Olteanu <olteanuflorin86@gmail.com>
2023-12-21 08:14:34 +0200

    implement book DAO

--- src/main/java/com/olteanuflorin86/sdjpaintro/dao/AuthorDaoImpl.java
+++ src/main/java/com/olteanuflorin86/sdjpaintro/dao/AuthorDaoImpl.java
@@ -19,48 +19,86 @@ public class AuthorDaoImpl implements AuthorDao {
 	
     @Override
     public Author getById(Long id) {
-        return getEntityManager().find(Author.class, id);
+//        return getEntityManager().find(Author.class, id);
+        EntityManager em = getEntityManager();
+        Author author = getEntityManager().find(Author.class, id);
+        em.close();
+        return author;
     }
 
     @Override
     public Author findAuthorByName(String firstName, String lastName) {
-    	TypedQuery<Author> query = getEntityManager().createQuery("SELECT a FROM Author a " + "WHERE a.firstName = :first_name and a.lastName = :last_name", Author.class);
+//    	TypedQuery<Author> query = getEntityManager().createQuery("SELECT a FROM Author a " + "WHERE a.firstName = :first_name and a.lastName = :last_name", Author.class);
+//        query.setParameter("first_name", firstName);
+//        query.setParameter("last_name", lastName);
+//        
+//    	return query.getSingleResult();
+        EntityManager em = getEntityManager();
+        TypedQuery<Author> query = em.createQuery("SELECT a FROM Author a " + "WHERE a.firstName = :first_name and a.lastName = :last_name", Author.class);
         query.setParameter("first_name", firstName);
         query.setParameter("last_name", lastName);
-        
-    	return query.getSingleResult();
+
+        Author author = query.getSingleResult();
+        em.close();
+        return author;
     }
 
     @Override
     public Author saveNewAuthor(Author author) {
-    	EntityManager em = getEntityManager();
-    	em.getTransaction().begin();
-    	em.persist(author);
-    	em.flush();
-    	em.getTransaction().commit();
-    	
+//    	EntityManager em = getEntityManager();
+//    	em.getTransaction().begin();
+//    	em.persist(author);
+//    	em.flush();
+//    	em.getTransaction().commit();
+//    	
+//        return author;
+        EntityManager em = getEntityManager();
+        em.getTransaction().begin();
+        em.persist(author);
+        em.flush();
+        em.getTransaction().commit();
+        em.close();
         return author;
     }
 
     @Override
     public Author updateAuthor(Author author) {
-    	EntityManager em = getEntityManager();
-    	em.joinTransaction();
-    	em.merge(author);
-    	em.flush();
-    	em.clear();
-    	
-        return em.find(Author.class, author.getId());
+//    	EntityManager em = getEntityManager();
+//    	em.joinTransaction();
+//    	em.merge(author);
+//    	em.flush();
+//    	em.clear();
+//    	
+//        return em.find(Author.class, author.getId());
+        EntityManager em = getEntityManager();
+
+        try {
+            em.joinTransaction();
+            em.merge(author);
+            em.flush();
+            em.clear();
+            Author saveAuthor = em.find(Author.class, author.getId());
+            return saveAuthor;
+        } finally {
+            em.close();
+        }
     }
 
     @Override
     public void deleteAuthorById(Long id) {
-    	EntityManager em = getEntityManager();
-    	em.getTransaction().begin();
-    	Author author = em.find(Author.class, id);
-    	em.remove(author);
-    	em.flush();
-    	em.getTransaction().commit();
+//    	EntityManager em = getEntityManager();
+//    	em.getTransaction().begin();
+//    	Author author = em.find(Author.class, id);
+//    	em.remove(author);
+//    	em.flush();
+//    	em.getTransaction().commit();
+        EntityManager em = getEntityManager();
+        em.getTransaction().begin();
+        Author author = em.find(Author.class, id);
+        em.remove(author);
+        em.flush();
+        em.getTransaction().commit();
+        em.close();
     }
 
     private EntityManager getEntityManager() {
--- src/test/java/com/olteanuflorin86/sdjpaintro/DaoIntegrationTest.java
+++ src/test/java/com/olteanuflorin86/sdjpaintro/DaoIntegrationTest.java
@@ -13,8 +13,10 @@ import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.test.context.ActiveProfiles;
 
 import com.olteanuflorin86.sdjpaintro.domain.Author;
+import com.olteanuflorin86.sdjpaintro.domain.Book;
 import com.olteanuflorin86.sdjpaintro.dao.AuthorDao;
 import com.olteanuflorin86.sdjpaintro.dao.AuthorDaoImpl;
+import com.olteanuflorin86.sdjpaintro.dao.BookDao;
 
 @ActiveProfiles("local")
 @DataJpaTest
@@ -26,74 +28,76 @@ public class DaoIntegrationTest {
     @Autowired
     AuthorDao authorDao;
 
-//    @Autowired
-//    BookDao bookDao;
-//
-//    @Test
-//    void testDeleteBook() {
-//        Book book = new Book();
-//        book.setIsbn("1234");
-//        book.setPublisher("Self");
-//        book.setTitle("my book");
-//        Book saved = bookDao.saveNewBook(book);
-//
-//        bookDao.deleteBookById(saved.getId());
-//
-//        Book deleted = bookDao.getById(saved.getId());
-//
-//        assertThat(deleted).isNull();
-//    }
-//
-//    @Test
-//    void updateBookTest() {
-//        Book book = new Book();
-//        book.setIsbn("1234");
-//        book.setPublisher("Self");
-//        book.setTitle("my book");
-//
-//        Author author = new Author();
-//        author.setId(3L);
-//
-//        book.setAuthor(author);
-//        Book saved = bookDao.saveNewBook(book);
-//
-//        saved.setTitle("New Book");
-//        bookDao.updateBook(saved);
-//
-//        Book fetched = bookDao.getById(saved.getId());
-//
-//        assertThat(fetched.getTitle()).isEqualTo("New Book");
-//    }
-//
-//    @Test
-//    void testSaveBook() {
-//        Book book = new Book();
-//        book.setIsbn("1234");
-//        book.setPublisher("Self");
-//        book.setTitle("my book");
-//
-//        Author author = new Author();
-//        author.setId(3L);
-//
-//        book.setAuthor(author);
-//        Book saved = bookDao.saveNewBook(book);
-//
-//        assertThat(saved).isNotNull();
-//    }
-//
-//    @Test
-//    void testGetBookByName() {
-//        Book book = bookDao.findBookByTitle("Clean Code");
-//
-//        assertThat(book).isNotNull();
-//    }
-//
-//    @Test
-//    void testGetBook() {
-//        Book book = bookDao.getById(3L);
-//
-//        assertThat(book.getId()).isNotNull();
-//    }
+    @Autowired
+    BookDao bookDao;
+
+    @Test
+    void testGetBook() {
+    	Book book = bookDao.getById(3L);
+
+    	assertThat(book.getId()).isNotNull();
+    }
+    
+    @Test
+    void testGetBookByName() {
+        Book book = bookDao.findBookByTitle("Clean Code");
+
+        assertThat(book).isNotNull();
+    }
+
+	@Test
+	void testSaveBook() {
+	    Book book = new Book();
+	    book.setIsbn("1234");
+	    book.setPublisher("Self");
+	    book.setTitle("my book");
+	
+	    Author author = new Author();
+	    author.setId(3L);
+	
+//	    book.setAuthor(author);
+	    book.setAuthorId(1L);
+	    Book saved = bookDao.saveNewBook(book);
+	
+	    assertThat(saved).isNotNull();
+	}
+
+	@Test
+  	void updateBookTest() {
+      	Book book = new Book();
+      	book.setIsbn("1234");
+      	book.setPublisher("Self");
+      	book.setTitle("my book");
+
+      	Author author = new Author();
+      	author.setId(3L);
+
+//      	book.setAuthor(author);
+      	book.setAuthorId(1L);
+      	Book saved = bookDao.saveNewBook(book);
+
+      	saved.setTitle("New Book");
+      	bookDao.updateBook(saved);
+
+      	Book fetched = bookDao.getById(saved.getId());
+
+      	assertThat(fetched.getTitle()).isEqualTo("New Book");
+  	}
+    
+    @Test
+    void testDeleteBook() {
+        Book book = new Book();
+        book.setIsbn("1234");
+        book.setPublisher("Self");
+        book.setTitle("my book");
+        Book saved = bookDao.saveNewBook(book);
+
+        bookDao.deleteBookById(saved.getId());
+
+        Book deleted = bookDao.getById(saved.getId());
+
+        assertThat(deleted).isNull();
+    }
 
 
 
