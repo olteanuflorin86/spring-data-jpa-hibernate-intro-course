ed3e36eea8753528480a24f20b3d47aafbcafbd3
Florin Olteanu <olteanuflorin86@gmail.com>
2023-12-22 10:01:41 +0200

    Initial setup

---
+++ src/main/java/com/olteanuflorin86/sdjpaintro/dao/AuthorDaoImpl.java
@@ -0,0 +1,40 @@
+package com.olteanuflorin86.sdjpaintro.dao;
+
+import org.springframework.stereotype.Component;
+
+import com.olteanuflorin86.sdjpaintro.domain.Author;
+
+@Component
+public class AuthorDaoImpl implements AuthorDao {
+
+	@Override
+	public Author getById(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Author findAuthorByName(String firstName, String lastName) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Author saveNewAuthor(Author author) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Author updateAuthor(Author author) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void deleteAuthorById(Long id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
--- src/main/java/com/olteanuflorin86/sdjpaintro/domain/Book.java
+++ src/main/java/com/olteanuflorin86/sdjpaintro/domain/Book.java
@@ -15,12 +15,13 @@ import jakarta.persistence.Id;
 public class Book {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
 	private String title;
 	private String isbn;
 	private String publisher;
+	private Long authorId;
 	
 	public Book() {
 	}
@@ -32,13 +33,13 @@ public class Book {
 		this.publisher = publisher;
 	}
 	
-	public Book(Long id, String title, String isbn, String publisher) {
-//		super();
-		this.id = id;
-		this.title = title;
-		this.isbn = isbn;
-		this.publisher = publisher;
-	}
+//	public Book(Long id, String title, String isbn, String publisher) {
+////		super();
+//		this.id = id;
+//		this.title = title;
+//		this.isbn = isbn;
+//		this.publisher = publisher;
+//	}
 
 	public Long getId() {
 		return id;
@@ -71,6 +72,14 @@ public class Book {
 	public void setPublisher(String publisher) {
 		this.publisher = publisher;
 	}
+	
+	public Long getAuthorId() {
+		return authorId;
+	}
+	
+	public void setAuthorId(Long authorId) {
+		this.authorId = authorId;
+	}
 
 	@Override
 	public int hashCode() {
@@ -88,7 +97,5 @@ public class Book {
 		Book other = (Book) obj;
 		return Objects.equals(id, other.id);
 	}
-	
-	
 	
 }
