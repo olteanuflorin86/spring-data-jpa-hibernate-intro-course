06a6120e6274455610511e0bbc777e21d6d125ba
Florin Olteanu <olteanuflorin86@gmail.com>
2023-12-27 13:29:25 +0200

    Initial setup - small update

--- src/test/java/com/olteanuflorin86/sdjpaintro/SdjpaIntroApplicationTests.java
+++ src/test/java/com/olteanuflorin86/sdjpaintro/SdjpaIntroApplicationTests.java
@@ -1,8 +1,10 @@
 package com.olteanuflorin86.sdjpaintro;
 
+import org.junit.jupiter.api.Disabled; 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Disabled // not configured...
 @SpringBootTest
 class SdjpaIntroApplicationTests {
 
