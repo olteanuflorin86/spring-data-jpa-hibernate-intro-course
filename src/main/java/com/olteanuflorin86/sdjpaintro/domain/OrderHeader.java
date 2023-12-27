0c14d52c30cb885b609f237922f9cbd45b90951b
Florin Olteanu <olteanuflorin86@gmail.com>
2023-12-27 14:04:12 +0200

    using JPA @MappedSuperclass inheritance option

--- src/main/java/com/olteanuflorin86/sdjpaintro/domain/OrderHeader.java
+++ src/main/java/com/olteanuflorin86/sdjpaintro/domain/OrderHeader.java
@@ -1,28 +1,26 @@
 package com.olteanuflorin86.sdjpaintro.domain;
 
-import java.util.Objects;
-
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+//import jakarta.persistence.GeneratedValue;
+//import jakarta.persistence.GenerationType;
+//import jakarta.persistence.Id;
 
 @Entity
-public class OrderHeader {
+public class OrderHeader extends BaseEntity {
 	
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
+//	@Id
+//	@GeneratedValue(strategy = GenerationType.IDENTITY)
+//	private Long id;
 	
 	private String customer;
 
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
+//	public Long getId() {
+//		return id;
+//	}
+//
+//	public void setId(Long id) {
+//		this.id = id;
+//	}
 
 	public String getCustomer() {
 		return customer;
@@ -32,22 +30,39 @@ public class OrderHeader {
 		this.customer = customer;
 	}
 
+//    @Override
+//    public boolean equals(Object o) {
+//        if (this == o) return true;
+//        if (o == null || getClass() != o.getClass()) return false;
+//
+//        OrderHeader that = (OrderHeader) o;
+//
+//        if (id != null ? !id.equals(that.id) : that.id != null) return false;
+//        return customer != null ? customer.equals(that.customer) : that.customer == null;
+//    }
+//
+//    @Override
+//    public int hashCode() {
+//        int result = id != null ? id.hashCode() : 0;
+//        result = 31 * result + (customer != null ? customer.hashCode() : 0);
+//        return result;
+//    }	
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (!(o instanceof OrderHeader)) return false;
+        if (!super.equals(o)) return false;
 
         OrderHeader that = (OrderHeader) o;
 
-        if (id != null ? !id.equals(that.id) : that.id != null) return false;
-        return customer != null ? customer.equals(that.customer) : that.customer == null;
+        return getCustomer() != null ? getCustomer().equals(that.getCustomer()) : that.getCustomer() == null;
     }
 
     @Override
     public int hashCode() {
-        int result = id != null ? id.hashCode() : 0;
-        result = 31 * result + (customer != null ? customer.hashCode() : 0);
+        int result = super.hashCode();
+        result = 31 * result + (getCustomer() != null ? getCustomer().hashCode() : 0);
         return result;
-    }	
-
+    }
 }
---
+++ src/test/java/com/olteanuflorin86/sdjpaintro/domain/OrderHeaderTest.java
@@ -0,0 +1,31 @@
+package com.olteanuflorin86.sdjpaintro.domain;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+import org.junit.jupiter.api.Test;
+
+public class OrderHeaderTest {
+
+    @Test
+    void testEquals() {
+        OrderHeader oh1 = new OrderHeader();
+        oh1.setId(1L);
+
+        OrderHeader oh2 = new OrderHeader();
+        oh2.setId(1L);
+
+        assert oh1.equals(oh2);
+    }
+
+    @Test
+    void testNotEquals() {
+        OrderHeader oh1 = new OrderHeader();
+        oh1.setId(1L);
+
+        OrderHeader oh2 = new OrderHeader();
+        oh2.setId(3L);
+
+        assertFalse( oh1.equals(oh2));
+    }
+    
+}
