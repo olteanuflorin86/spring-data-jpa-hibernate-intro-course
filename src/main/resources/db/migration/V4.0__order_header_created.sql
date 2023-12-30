50c0ea75a0a09740c6b77ac21d4d9aa9985cfc8d
Florin Olteanu <olteanuflorin86@gmail.com>
2023-12-30 11:10:22 +0200

    create two separate Flyway migration scripts for create and update

---
+++ src/main/resources/db/migration/V4.0__order_header_created.sql
@@ -0,0 +1,3 @@
+alter table order_header
+    add column created_date TIMESTAMP;
+    
--- src/main/resources/db/migration/V5.0__order_header_updated.sql
+++ src/main/resources/db/migration/V5.0__order_header_updated.sql
@@ -1,5 +1,2 @@
 alter table order_header
-    add column created_date TIMESTAMP;
-    
- alter table order_header
     add column last_modified_date TIMESTAMP DEFAULT NOW();
