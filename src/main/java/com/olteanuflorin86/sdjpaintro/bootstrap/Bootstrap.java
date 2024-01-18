c9e576b274c213442da07f248d64298845677c18
Florin Olteanu <olteanuflorin86@gmail.com>
2024-01-18 17:54:49 +0200

    LAZY Initialization error example

--- src/main/java/com/olteanuflorin86/sdjpaintro/bootstrap/Bootstrap.java
+++ src/main/java/com/olteanuflorin86/sdjpaintro/bootstrap/Bootstrap.java
@@ -1,15 +1,33 @@
 package com.olteanuflorin86.sdjpaintro.bootstrap;
 
+import org.springframework.beans.factory.annotation.Autowired; 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.olteanuflorin86.sdjpaintro.repositories.OrderHeaderRepository;
+import com.olteanuflorin86.sdjpaintro.domain.OrderHeader;
+
 
 @Component
 public class Bootstrap implements CommandLineRunner {
 
+	@Autowired
+	OrderHeaderRepository orderHeaderRepository;
+	
+	@Transactional
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println("I was called!");
+//		System.out.println("I was called!");
+		OrderHeader orderHeader = orderHeaderRepository.findById(1L).get();
 		
+		orderHeader.getOrderLines().forEach(ol -> {
+			System.out.println(ol.getProduct().getDescription());
+			
+			ol.getProduct().getCategories().forEach(cat -> {
+				System.out.println(cat.getDescription());
+			});
+		});
 	}
 
 }
