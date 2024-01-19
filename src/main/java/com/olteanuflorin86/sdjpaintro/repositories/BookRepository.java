ce5040c2d5f7f921fa09a0248542462ce9645c5f
Florin Olteanu <olteanuflorin86@gmail.com>
2024-01-19 20:25:02 +0200

    Initial setup

--- src/main/java/com/olteanuflorin86/sdjpaintro/repositories/BookRepository.java
+++
@@ -1,9 +0,0 @@
-package com.olteanuflorin86.sdjpaintro.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.olteanuflorin86.sdjpaintro.domain.Book;
-
-public interface BookRepository extends JpaRepository<Book, Long> {
-
-}
