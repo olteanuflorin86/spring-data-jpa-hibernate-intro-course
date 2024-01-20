8e3b600994bf726f2bee47beabdd1052530a228d
Florin Olteanu <olteanuflorin86@gmail.com>
2024-01-20 06:55:12 +0200

    using @Inheritance with SINGLE_TABLE example

---
+++ src/main/java/com/olteanuflorin86/sdjpaintro/domain/singletable/Truck.java
@@ -0,0 +1,20 @@
+package com.olteanuflorin86.sdjpaintro.domain.singletable;
+
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+
+@Entity
+@DiscriminatorValue("truck")
+public class Truck extends Vehicle {
+
+	private Integer payload;
+
+	public Integer getPayload() {
+		return payload;
+	}
+
+	public void setPayload(Integer payload) {
+		this.payload = payload;
+	}
+	
+}
---
+++ src/main/java/com/olteanuflorin86/sdjpaintro/domain/singletable/Vehicle.java
@@ -0,0 +1,28 @@
+package com.olteanuflorin86.sdjpaintro.domain.singletable;
+
+import jakarta.persistence.DiscriminatorColumn;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
+
+@Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "vehicle_type")
+public class Vehicle {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+}
