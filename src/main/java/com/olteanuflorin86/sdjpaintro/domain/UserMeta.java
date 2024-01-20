0faa34db00319129f56eaafa6ed77df1639c258f
Florin Olteanu <olteanuflorin86@gmail.com>
2024-01-20 11:36:50 +0200

    add UserMeta entity and map it to wp_usermeta table + use @Lob

---
+++ src/main/java/com/olteanuflorin86/sdjpaintro/domain/UserMeta.java
@@ -0,0 +1,59 @@
+package com.olteanuflorin86.sdjpaintro.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "wp_usermeta")
+public class UserMeta {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "umeta_id")
+	private Long id;
+    
+    private Long userId;
+	
+	private String metaKey;
+	
+	@Lob
+	private String metaValue;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Long getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
+
+	public String getMetaKey() {
+		return metaKey;
+	}
+
+	public void setMetaKey(String metaKey) {
+		this.metaKey = metaKey;
+	}
+
+	public String getMetaValue() {
+		return metaValue;
+	}
+
+	public void setMetaValue(String metaValue) {
+		this.metaValue = metaValue;
+	}
+	
+}
