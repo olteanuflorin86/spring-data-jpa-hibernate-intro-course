ae141c3bd16d076c6fa96ed935da2cc665dfbe5a
Florin Olteanu <olteanuflorin86@gmail.com>
2024-01-24 11:28:02 +0200

    adding encryption decryption to listener

--- src/main/java/com/olteanuflorin86/sdjpaintro/domain/CreditCardJPACallback.java
+++ src/main/java/com/olteanuflorin86/sdjpaintro/domain/CreditCardJPACallback.java
@@ -1,5 +1,8 @@
 package com.olteanuflorin86.sdjpaintro.domain;
 
+import com.olteanuflorin86.sdjpaintro.config.SpringContextHelper;
+import com.olteanuflorin86.sdjpaintro.services.EncryptionService;
+
 import jakarta.persistence.PostLoad;
 import jakarta.persistence.PostPersist;
 import jakarta.persistence.PostUpdate;
@@ -12,6 +15,8 @@ public class CreditCardJPACallback {
 	@PreUpdate
 	public void beforeInsertOrUpdate(CreditCard creditCard) {
 		System.out.println("before update was called...");
+		
+		creditCard.setCreditCardNumber(getEncryptionService().encrypt(creditCard.getCreditCardNumber()));
 	}
 	
 	@PostPersist
@@ -19,6 +24,12 @@ public class CreditCardJPACallback {
 	@PostLoad
 	public void postLoad(CreditCard creditCard) {
 		System.out.println("Post Load was called...");
+		
+		creditCard.setCreditCardNumber(getEncryptionService().decrypt(creditCard.getCreditCardNumber()));
 	}
+	
+    private EncryptionService getEncryptionService(){
+        return SpringContextHelper.getApplicationContext().getBean(EncryptionService.class);
+    }
 	
 }
