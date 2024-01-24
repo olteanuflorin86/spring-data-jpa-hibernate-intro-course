f7d9293eedf72a503e9c144f4df6188f42527c08
Florin Olteanu <olteanuflorin86@gmail.com>
2024-01-24 17:41:26 +0200

    adding project lombok

--- src/main/java/com/olteanuflorin86/sdjpaintro/domain/cardholder/CreditCardHolder.java
+++ src/main/java/com/olteanuflorin86/sdjpaintro/domain/cardholder/CreditCardHolder.java
@@ -1,11 +1,21 @@
 package com.olteanuflorin86.sdjpaintro.domain.cardholder;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.Entity; 
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.Builder;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class CreditCardHolder {
 	
     @Id
--- src/main/java/com/olteanuflorin86/sdjpaintro/domain/creditcard/CreditCard.java
+++ src/main/java/com/olteanuflorin86/sdjpaintro/domain/creditcard/CreditCard.java
@@ -1,14 +1,24 @@
 package com.olteanuflorin86.sdjpaintro.domain.creditcard;
 
-import jakarta.persistence.Convert;
+import jakarta.persistence.Convert; 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import com.olteanuflorin86.sdjpaintro.domain.CreditCardConverter;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class CreditCard {
 
     @Id
