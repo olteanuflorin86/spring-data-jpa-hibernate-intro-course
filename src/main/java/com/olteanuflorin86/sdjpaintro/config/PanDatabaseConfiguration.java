dfb793230c0e103f677898c7d63141d5ef7d9ffb
Florin Olteanu <olteanuflorin86@gmail.com>
2024-01-27 18:36:27 +0200

    adding datasource properties

---
+++ src/main/java/com/olteanuflorin86/sdjpaintro/config/PanDatabaseConfiguration.java
@@ -0,0 +1,18 @@
+package com.olteanuflorin86.sdjpaintro.config;
+
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+@Configuration
+public class PanDatabaseConfiguration {
+
+	@Bean
+	@Primary
+	@ConfigurationProperties("spring.pan.datasource")
+	public DataSourceProperties panDataSourceProperties() {
+		return new DataSourceProperties();
+	}
+}
