2d186f7721d7f6523316c8d020f3a300729926c8
Florin Olteanu <olteanuflorin86@gmail.com>
2024-01-29 17:19:17 +0200

    fix entity/table naming convention

--- src/main/java/com/olteanuflorin86/sdjpaintro/domain/pan/CreditCardPAN.java
+++ src/main/java/com/olteanuflorin86/sdjpaintro/domain/pan/CreditCardPAN.java
@@ -5,6 +5,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -19,6 +20,7 @@ import com.olteanuflorin86.sdjpaintro.domain.CreditCardConverter;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@Table(name = "credit_card_pan")
 public class CreditCardPAN {
 
     @Id
