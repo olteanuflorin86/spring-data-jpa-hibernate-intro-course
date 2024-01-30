13f4c2dd325eb1021a6cbd1fb314d44309be32e3
Florin Olteanu <olteanuflorin86@gmail.com>
2024-01-30 16:06:50 +0200

    adding transient properties

--- src/main/java/com/olteanuflorin86/sdjpaintro/domain/creditcard/CreditCard.java
+++ src/main/java/com/olteanuflorin86/sdjpaintro/domain/creditcard/CreditCard.java
@@ -5,6 +5,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Transient;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -31,4 +32,15 @@ public class CreditCard {
     @Convert(converter = CreditCardConverter.class)
     private String expirationDate;
 
+    @Transient
+    private String creditCardNumber;
+    
+    @Transient
+    private String firstName;
+    
+    @Transient
+    private String lastName;
+    
+    @Transient
+    private String zipCode;
 }
