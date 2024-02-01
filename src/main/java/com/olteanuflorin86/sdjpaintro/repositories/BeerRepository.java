63a613b96becf0d2577d8fa1afe7cb79b3d2eefc
Florin Olteanu <olteanuflorin86@gmail.com>
2024-02-01 20:11:15 +0200

    set entity path

--- src/main/java/com/olteanuflorin86/sdjpaintro/repositories/BeerRepository.java
+++ src/main/java/com/olteanuflorin86/sdjpaintro/repositories/BeerRepository.java
@@ -1,14 +1,16 @@
 package com.olteanuflorin86.sdjpaintro.repositories;
 
-import java.util.UUID; 
+import java.util.UUID;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.olteanuflorin86.sdjpaintro.domain.Beer;
 import com.olteanuflorin86.sdjpaintro.domain.BeerStyleEnum;
 
+@RepositoryRestResource(path = "beer", collectionResourceRel = "beer")
 public interface BeerRepository extends JpaRepository<Beer, UUID> {
 
 	Page<Beer> findAllByBeerName(String beerName, Pageable pageable);
