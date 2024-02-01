d0e5fe76b19bbbf7e904a0900c52e2fe815bcbf0
Florin Olteanu <olteanuflorin86@gmail.com>
2024-02-01 19:24:50 +0200

    Initial setup

---
+++ src/main/java/com/olteanuflorin86/sdjpaintro/domain/BeerStyleEnum.java
@@ -0,0 +1,6 @@
+package com.olteanuflorin86.sdjpaintro.domain;
+
+public enum BeerStyleEnum {
+
+	LAGER, PILSNER, STOUT, GOSE, PORTER, ALE, WHEAT, IPA, PALE_ALE, SAISON
+}
---
+++ src/main/java/com/olteanuflorin86/sdjpaintro/domain/Customer.java
@@ -0,0 +1,19 @@
+package com.olteanuflorin86.sdjpaintro.domain;
+
+import java.util.UUID;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Customer {
+
+	private UUID id;
+	
+	private String name;
+}
